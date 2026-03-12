0bbc6bcc9fbdc49aae06aa732617d90daf883327
AnshikaXhack <148768843+AnshikaXhack@users.noreply.github.com>
2026-03-12 18:53:30 +0530

    update db connection

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,14 +1,15 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectToDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ error in connecting with db", error);
   }
 };
 
-module.exports = connectToDb;
+export default connectToDb;
